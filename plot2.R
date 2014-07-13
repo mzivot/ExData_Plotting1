006460f665789bce31a7ddae2480436677772f6f
Matthew Zivot <mzivot@gmail.com>
2014-07-13 17:22:58 -0400

    added graphs and  R scripts that created them

---
+++ plot2.R
@@ -0,0 +1,16 @@
+#Exploratory Analysis Course Project 1
+#setwd("/Users/student/Documents/Classes/JHDataScience/ExploratoryAnalysis/Project1/Graphs/ExData_Plotting1")
+#If zipped data file doesn't exist, download it and unzip it
+fileUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (!file.exists("household_power_consumption.txt")) {
+    fileUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileUrl,destfile ="./zipdata.zip",method = "curl")
+    unzip("zipdata.zip")
+}
+ColNames = as.vector(as.matrix(read.table("household_power_consumption.txt",header=FALSE,sep=";",na.strings="?",nrows = 1))) #get header
+data = read.table("household_power_consumption.txt",header=FALSE,sep=";",col.names = ColNames,na.strings="?",skip=66637,nrows = 2880) #read in selected data
+attach(data)
+dateTime = strptime(paste(data$Date,data$Time), format= "%d/%m/%Y %H:%M:%S")
+png(filename = "plot2.png",width = 480, height = 480) #open png file to save graph
+plot(dateTime,Global_active_power,type = "l",xlab = "",ylab="Global Active Power (kilowatts)") #create graph
+dev.off() #close png file
---
+++ plot4.R
@@ -0,0 +1,23 @@
+#Exploratory Analysis Course Project 1
+#setwd("/Users/student/Documents/Classes/JHDataScience/ExploratoryAnalysis/Project1/Graphs/ExData_Plotting1")
+#If zipped data file doesn't exist, download it and unzip it
+fileUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (!file.exists("household_power_consumption.txt")) {
+    fileUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileUrl,destfile ="./zipdata.zip",method = "curl")
+    unzip("zipdata.zip")
+}
+ColNames = as.vector(as.matrix(read.table("household_power_consumption.txt",header=FALSE,sep=";",na.strings="?",nrows = 1))) #get header
+data = read.table("household_power_consumption.txt",header=FALSE,sep=";",col.names = ColNames,na.strings="?",skip=66637,nrows = 2880) #read in selected data
+attach(data)
+dateTime = strptime(paste(data$Date,data$Time), format= "%d/%m/%Y %H:%M:%S")
+png(filename = "plot4.png",width = 480, height = 480) #open png file to save graph
+par(mfrow = c(2,2),mar = c(4,4,2,2))
+plot(dateTime,Global_active_power,type = "l",xlab = "",ylab="Global Active Power") #create graph
+plot(dateTime,Voltage,type = "l",xlab = "datetime") #create graph
+plot(dateTime,Sub_metering_1,type = "l",xlab = "",ylab="Energy sub metering") #create graph
+lines(dateTime,Sub_metering_2,col = "red", type = "l",xlab = "",ylab="Global Active Power (kilowatts)")
+lines(dateTime,Sub_metering_3,col = "blue", type = "l",xlab = "",ylab="Global Active Power (kilowatts)")
+legend("topright",col = c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),pch="-",bty="n")
+plot(dateTime,Global_reactive_power,type = "l",xlab = "datetime") #create graph
+dev.off() #close png file
